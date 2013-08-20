50a37e72303faf3f8ef0a9186740d0ad83020a12
Jan Antala <jan@antala.sk>
2013-08-20 14:17:01 +0200

    test conf update

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,12 +1,15 @@
-basePath = '..';
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'https://maps.googleapis.com/maps/api/js?sensor=false',
-  'src/adaptive-googlemaps.js',
-  'test/*.spec.js'
-];
-singleRun = true;
-browsers = [ 'Chrome' ];
+module.exports = function(config) {
+  config.set({
+    basePath: '..',
+    files: [
+      'components/angular/angular.js',
+      'components/angular-mocks/angular-mocks.js',
+      'https://maps.googleapis.com/maps/api/js?sensor=false',
+      'src/adaptive-googlemaps.js',
+      'test/*.spec.js'
+    ],
+    frameworks: ['jasmine'],
+    singleRun: true,
+    browsers: [ 'Chrome' ]
+  });
+};
